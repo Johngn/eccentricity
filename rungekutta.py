3e2f944080a1b28184506c53743017058c9b7a44
john <jgillan@gmail.com>
2020-06-21 15:31:09 +0200

    second runge kutta

--- rungekutta.py
+++ rungekutta.py
@@ -17,27 +17,24 @@ year = 365.25*24.*60.*60.
 au = 1.496e11
 m = 1.99e30
 
-# G = 4*np.pi**2
-# mstar = 1
-# mplanet = 3e-6
-# dt = 0.001
-# a = 1
-# lim = 2
+G = 4*np.pi**2
+mstar = 1
+mplanet = 3e-6
+dt = 0.001
+a = 1
+lim = 2
 
 G = 6.674e-11
 mstar = 1.99e30
 mplanet = 6.0e24
 dt = year*0.001
 a = au
-lim = au*2
+lim = au*5
 
-noutputs = 3000000
-totaltime = noutputs*dt/year
+noutputs = 30
+totaltime = noutputs*dt
 
 h = 0.05
-
-# initial position and velocity of star and planet [x,y,z,vx,vy,vz]
-W0 = np.array([0,r_a,0,-vorb,0,0])
 
 e = 0.3
 ehat = e/h
@@ -58,37 +55,43 @@ tau_m = (0.5/tau_a-e**2/tau_e)**-1
 
 vorb = np.sqrt(G*mstar*(2/r_a-1/a))
 
-
-
+W0 = np.array([0,r_a,0,-vorb,0,0])
 # %%
 # force equation
-def force(W0):
-    x = W0[0:3]
-    v = W0[3:6]
+def force(W):
+    x = W[0:3]
+    v = W[3:6]
     r = np.linalg.norm(x)
-    vmag = np.linalg.norm(v)
     
     dvdt1 = -G*mstar*x/r**3
+    
+    # print(dvdt1)
+    
+    return np.hstack((v, dvdt1))
+
+def force2(W):
+    x = W[0:3]
+    v = W[3:6]
+    r = np.linalg.norm(x)
+    vmag = np.linalg.norm(v)
     
     u_norm = x/r
     u_tang = np.array([-u_norm[1], u_norm[0], 0])
     v_norm = np.dot(u_norm, v)
     v_tang = np.dot(u_tang, v)
     
-    dvdt2 = -2*(np.dot(v,x)*x)/r**2/tau_e
+    # dvdt2 = -2*(np.dot(v,x)*x)/r**2/tau_e
     # print(dvdt2)
-    dvdt2 = -v/tau_m-2*v_norm/tau_e*u_norm
+    # dvdt2 = -v/tau_m-2*v_norm/tau_e*u_norm
     # print(dvdt2)
-    dvdt2 = -vmag/2/tau_a*u_tang-v_norm/tau_e*u_norm-(v_tang-vmag)/tau_e*u_tang    
+    dvdt2 = -vmag/2/tau_a*u_tang-v_norm/tau_e*u_norm-(v_tang-vmag)/tau_e*u_tang
+    
     # print(dvdt2)
     
-    W = np.hstack((v, dvdt1+dvdt2))
-    print(dvdt1)
-    return W
+    return np.hstack((v, dvdt2))
 
-# runge kutta integrator
 def rungekutta(W0):
-    W = np.zeros((noutputs, 1, 6))
+    W = np.zeros((noutputs,6))
     for i in range(noutputs):
         fa = force(W0)
         Wb = W0 + dt/2*fa
@@ -98,11 +101,43 @@ def rungekutta(W0):
         Wd = W0 + dt*fc
         fd = force(Wd)    
         W0 = W0 + dt/6*fa + dt/3*fb + dt/3*fc + dt/6*fd
+        
+        # print(W0)
+        
+        fa = force2(W0)
+        Wb = W0 + dt/2*fa
+        fb = force2(Wb)
+        Wc = W0 + dt/2*fb
+        fc = force2(Wc)
+        Wd = W0 + dt*fc
+        fd = force2(Wd)    
+        W0 = W0 + dt/6*fa + dt/3*fb + dt/3*fc + dt/6*fd
+        
+        # print(W0)
+        
         W[i] = W0
-    return W    
+        
+    return W
+
+def rungekutta2(W0):
+    W = np.zeros((noutputs,6))
+    for i in range(noutputs):        
+        fa = force2(W0)
+        Wb = W0 + dt/2*fa
+        fb = force2(Wb)
+        Wc = W0 + dt/2*fb
+        fc = force2(Wc)
+        Wd = W0 + dt*fc
+        fd = force2(Wd)    
+        W0 = W0 + dt/6*fa + dt/3*fb + dt/3*fc + dt/6*fd
+        
+        print(W0[3])
+        
+        W[i] = W0
+    return W
 
 timer = timed()
-W = rungekutta(W0) # 6 dimensional phase space for each particle at each timestep
+W = rungekutta(W0)
 print(timed()-timer)
 # %%
 times = np.linspace(0,noutputs*dt,noutputs) # for displaying time elapsed in plot
@@ -122,13 +157,12 @@ text = ax.text(-lim+0.1, lim-0.2, s='', fontsize=15)
 
 def animate(i):
     star.set_data(0,0)
-    planet.set_data(W[i,0,0],W[i,0,1])
-    planetline.set_data(W[0:i,0,0],W[0:i,0,1])
+    planet.set_data(W[i,0],W[i,1])
+    planetline.set_data(W[0:i,0],W[0:i,1])
     text.set_text('{:.1f} years'.format(times[i]))
     return star, planet, planetline, text
     
 anim = animation.FuncAnimation(fig, animate, frames=noutputs, interval=1, blit=True)
-# anim.save('solar.gif')
 # %%
 fig, ax = plt.subplots(1, figsize=(8, 8))
 ax.set_xlim(-lim, lim)
@@ -138,6 +172,6 @@ ax.set_xlabel('Distance (AU)')
 ax.set_ylabel('Distance (AU)')
 ax.grid()
 ax.scatter(0,0)
-ax.scatter(W[-1,0,0], W[-1,0,1], c='orange')
-ax.plot(W[:,0,0], W[:,0,1], linewidth=0.1, c='orange')
-# plt.savefig('solar0001.png', bbox_inches='tight')
+ax.scatter(W[-1,0], W[-1,1], c='orange')
+ax.plot(W[:,0], W[:,1], linewidth=0.1, c='orange')
+# plt.savefig('rg.png', bbox_inches='tight')
