a5880d9462d070d2c22ac5e8c0c01bff2f9446e3
John Gillan <jgillan@protonmail.com>
2020-07-26 21:02:20 +0200

    added genga data

--- ida2020plots.py
+++ ida2020plots.py
@@ -22,11 +22,11 @@ r = 1*au   # radial position of planet
 
 h = 0.05                                    # scale height of disc
 
-i = 0
-e = np.arange(0, 0.5, 0.005)                # eccentricites
+# i = 0
+# e = np.arange(0, 0.5, 0.005)                # eccentricites
 
-# e = 0
-# i = np.arange(0, h*10, 0.0001)              # inclinations
+e = 0
+i = np.arange(0, h*10, 0.0001)              # inclinations
 
 ehat = e/h                                  # eccentricity divided by scale height of disc
 ihat = i/h                                  # inclinations divided by scale height of disc
@@ -36,38 +36,38 @@ sigma = 17000*(r/au)**(-3/2)                              # surface density of d
 t_wave = mstar/mplanet*mstar/sigma/r**2*h**(4)/omegak   # equation 7 from Ida 2020
 
 # equations for tau_e, tau_a, and tau_m by Cresswell & Nelson 2008 as taken from Ida 2020
-tau_e_CN = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3)                                                    # equation 17 from Ida 2020
-tau_m_CN = t_wave*2/(2.7+1.1*0.5)/h**2*(1+(ehat/2.25)**0.5+(ehat/2.84)**6)/(1-(ehat/2.02)**4)           # equation 18 from Ida 2020
-tau_a_CN = (2/tau_m_CN+2*e**2/tau_e_CN)**(-1)                                                               # equation 16 from Ida 2020    
+t_e_CN = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)
+t_i_CN = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)
+Pe = (1+(ehat/2.25)**1.2+(ehat/2.84)**6)/(1-(ehat/2.02)**4)
+t_a_CN = t_wave*2/h**2/(2.7+1.1*0.5)*(Pe+Pe/np.abs(Pe)*(0.07*ihat+0.085*ihat**4-0.08*ehat*ihat**2))                                                           # equation 16 from Ida 2020    
 
 Cm = 21.
 Ct = 4.25
 
 # equations for tau_e, tau_a, tau_m and tau_i by Ida 2020
-tau_e = t_wave/0.780*(1+1/15*(ehat**2+ihat**2)**(3/2))          # equation D1 in Ida 2020
-tau_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))          # equation D2 in Ida 2020
-tau_a = t_wave/Ct/h**2*(1+Ct/Cm*(ehat**2+ihat**2)**(1/2))         # equation D4 in Ida 2020
-tau_m = (0.5/tau_a-e**2/tau_e-i**2/tau_i)**(-1)                                 # equation D3 in Ida 2020
+t_e = t_wave/0.780*(1+1/15*(ehat**2+ihat**2)**(3/2))
+t_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))
+t_a = t_wave/Ct/h**2*(1+Ct/Cm*(ehat**2+ihat**2)**(1/2))
 # %%
 fig, ax = plt.subplots(2, figsize=(7,7))
 
-x = ehat
+x = ihat
 
-ax[0].plot(x, tau_i/year, label='IDA20')
-# ax[0].plot(x, tau_e_CN/year, label='CN08')
+ax[0].plot(x, t_i/year, label='IDA20')
+ax[0].plot(x, t_i_CN/year, label='CN08')
 # ax[0].plot(x, tau_e/year)
 ax[0].set_xscale('log')
 ax[0].set_yscale('log')
-ax[0].set_ylabel(r'$\tau_e$ (years)')
+ax[0].set_ylabel(r'$t_i$ (years)')
 # ax[0].set_ylabel(r'$\tau^{-1}_e/t_{wave}^{-1}$')
 ax[0].set_xlim(0.1, 10)
 # ax[0].set_ylim(0.001, 10)
 ax[0].tick_params(which='both', direction="in", top=True, right=True)
 ax[0].legend()
 
-ax[1].plot(x, tau_a/year, label='IDA20')
-ax[1].plot(x, tau_a_CN/year, label='CN08')
-# ax[1].plot(x, tau_a/year)
+ax[1].plot(x, t_a/year, label='IDA20')
+ax[1].plot(x, t_a_CN/year, label='CN08')
+# ax[1].plot(x, t_a/year)
 ax[1].set_xscale('log')
 ax[1].set_yscale('log')
 ax[1].set_xlabel('e/h')
--- rungekutta.py
+++ rungekutta.py
@@ -15,110 +15,81 @@ from timeit import default_timer as timed
 
 year = 365.25*24.*60.*60.
 au = 1.496e11
-solarmass = 1.989e30
-
+mstarkg = 1.989e30
 G = 4*np.pi**2
 mstar = 1
 mplanet = 3e-5
 dt = 0.02
 r = 1
-sigma = 17000*r**(-1/2)*au**2/solarmass
-lim = 1.5
-
-# G = 6.674e-11
-# mstar = 1.989e30
-# mplanet = 5.972e25
-# dt = year*0.02
-# r = au*5
-# sigma = 17000*(r/au)**(-3/2)
-# lim = au*1.5
-
+sigma = 17000*r**(-1/2)*au**2/mstarkg
+lim = 1.2
 mu = G*mstar
-noutputs = 20000
+noutputs = 100000
 totaltime = noutputs*dt
 h = 0.05
-i = 0.07
-ihat = i/h
-
 e = 0.0
+i = 0.07
 ehat = e/h
+ihat = i/h
 r_p = r*(1-e) # perihelion
 r_a = r*(1+e) # aphelion
-
-y = r_a*np.cos(i)
-z = r_a*np.sin(i)
-
 Cm = 21.
 Ct = 4.25
-
-vorb0 = np.sqrt(mu*(2/r_a-1/r)) # instantaneous orbital speed at aphelion
-
-W0 = np.array([0,y,z,-vorb0,0,0])
-
+y = r_a*np.cos(i)
+z = r_a*np.sin(i)
+vorb = np.sqrt(mu*(2/r_a-1/r)) # instantaneous orbital speed at aphelion
+W0 = np.array([0,y,z,-vorb,0,0])
+omegak = np.sqrt(mu/r**3)
+t_wave = mstar/mplanet*mstar/sigma/r**2*h**4/omegak
+# %%
 def acceleration(W0):
     R = W0[0:3]                             # position vector
     V = W0[3:6]                             # velocity vector
     r = np.linalg.norm(R)                   # magnitude of position vector
     v = np.linalg.norm(V)                   # magnitude of velocity vector    
-    
-    radius = np.sqrt(R[0]**2+R[1]**2)
-    vk = np.sqrt(mu/r)                 # instantaneous keplerian velocity
-    # print(radius)
-    # print(r)
-    
-    # vradius = np.sqrt(V[0]**2+V[1]**2)
-    # vtheta = np.arctan2(V[1],V[0])
-    # vz = V[2]
-    
-    # Rpolar = np.array([radius, theta, z])
-    # Vpolar = np.array([vradius, vtheta, vz])
+    dvdtG = -mu*R/r**3                      # acceleration due to gravity
+    vk = np.sqrt(mu/r)                      # instantaneous keplerian velocity
+    omegak = np.sqrt(mu/r**3)               
+    t_wave = mstar/mplanet*mstar/sigma/r**2*h**4/omegak
     
     theta = np.arctan2(R[1],R[0])
+    # print(v)
+    # print(vk)
     
-    u_r = np.array([np.cos(theta), np.sin(theta), 0])                           # radial unit vector
-    # u_a = -np.sin(theta)*np.array([1,0,0])+np.cos(theta)*np.array([0,1,0])    # azimuthal unit vector
-    u_a = np.array([-np.sin(theta), np.cos(theta), 0])
+    u_r = np.array([np.cos(theta), np.sin(theta), 0])       # radial unit vector
+    u_a = np.array([-np.sin(theta), np.cos(theta), 0])      # azimuthal unit vector
     u_z = np.array([0,0,1])
 
     vr = np.dot(V,u_r)                     # radial velocity
-    va = np.dot(V,u_a)                   # tangential velocity
+    va = np.dot(V,u_a)                   # azimuthal velocity
     vz = np.dot(V,u_z)
-    # print(vz)
-    # print(u_a)
-    # print(thetahat)
     
-    dvdtG = -mu*R/r**3                      # acceleration due to gravity
+    # print(u_r)
+    # print(V-(vr*u_r+va*u_a))
      
-    H = np.cross(R,V)
-    i = np.arccos(H[2]/np.linalg.norm(H))
-    ihat = i/h
-    
-    e = np.linalg.norm((v**2/mu - 1/r)*R - np.dot(R,V)/mu*V) # eccentricity
-    ehat = e/h                   
-    
-    omegak = np.sqrt(mu/r**3)               # new omegak every timestep
-    t_wave = mstar/mplanet*mstar/sigma/r**2*h**4/omegak # new t_wave every timestep
+    L = np.cross(R,V)
+    i = np.arccos(L[2]/np.linalg.norm(L))
+    ihat = i/h    
+    e = np.linalg.norm((v**2/mu-1/r)*R-np.dot(R,V)/mu*V) # eccentricity
+    ehat = e/h    
     
     # Ida prescription
-    tau_e = t_wave/0.780*(1+1/15*(ehat**2+ihat**2)**(3/2))
-    tau_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))
-    tau_a = t_wave/Ct/h**2*(1+Ct/Cm*(ehat**2+ihat**2)**(1/2))
+    # t_a = t_wave/Ct/h**2*(1+Ct/Cm*(ehat**2+ihat**2)**(1/2))
+    # t_e = t_wave/0.78*(1+1/15*(ehat**2+ihat**2)**(3/2))
+    # t_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))
+    # dvdt2 = - vk/2/t_a*u_a - vr/t_e*u_r - (va-vk)/t_e*u_a - vz/t_i*u_z
     
     # CN prescription
-    # tau_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)
-    # tau_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)
-    # Pe = (1+(ehat/2.25)**1.2+(ehat/2.84)**6)/(1-(ehat/2.02)**4)
-    # tau_a = t_wave*2/h**2/(2.7+1.1*0.5)*(Pe+Pe/np.abs(Pe)*(0.07*ihat+0.085*ihat**4-0.08*ehat*ihat**2))
-    
-    dvdt2 = -vk/2/tau_a*u_a - vr/tau_e*u_r - (va-vk)/tau_e*u_a - vz/tau_i*u_z  # equation 46 in Ida 2020
-    # dvdt2 = -V/tau_a -2*np.dot(V,R)*R/r**2/tau_e -vz/tau_i*zhat # equation 15 in Creswell+Nelson 2008
+    Pe = (1+(ehat/2.25)**1.2+(ehat/2.84)**6)/(1-(ehat/2.02)**4)
+    t_a = t_wave*2/h**2/(2.7+1.1*0.5)*(Pe+Pe/np.abs(Pe)*(0.07*ihat+0.085*ihat**4-0.08*ehat*ihat**2))
+    t_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)
+    t_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)    
+    dvdt2 = - V/t_a - 2*np.dot(V,R)*R/r**2/t_e - vz/t_i*u_z
     
     return np.hstack((V, dvdtG+dvdt2))
 
 def rungekutta(W0):
-    
     W = np.zeros((noutputs,6))
-    
     for i in range(noutputs):
         fa = acceleration(W0)
         Wb = W0 + dt/2*fa
@@ -128,73 +99,62 @@ def rungekutta(W0):
         Wd = W0 + dt*fc
         fd = acceleration(Wd)    
         W0 = W0 + dt/6*fa + dt/3*fb + dt/3*fc + dt/6*fd
-        
         W[i] = W0
-        
     return W
 
 timer = timed()
 W = rungekutta(W0)
 print(timed()-timer)
 
-omegak = np.sqrt(mu/r**3)
-t_wave = mstar/mplanet*mstar/sigma/r**2*h**4/omegak
-
 R = W[:,0:3]
 V = W[:,3:6]
+theta = np.arctan2(R[:,1],R[:,0])
 r = np.linalg.norm(R, axis=1)
 v = np.linalg.norm(V, axis=1)
-
-# e_total_low = np.linalg.norm([ (v[i]**2/mu - 1/r[i])*R[i] - (np.dot(R[i],V[i])*V[i])/mu for i in range(len(W)) ], axis=1)
-# e_total_mid = np.linalg.norm([ (v[i]**2/mu - 1/r[i])*R[i] - (np.dot(R[i],V[i])*V[i])/mu for i in range(len(W)) ], axis=1)
-e_total_high = np.linalg.norm([ (v[i]**2/mu - 1/r[i])*R[i] - (np.dot(R[i],V[i])*V[i])/mu for i in range(len(W)) ], axis=1)
-
-H = np.cross(R,V)
-i_total = np.arccos(H[:,2]/np.linalg.norm(H, axis=1))
-
-
-
+e_total = np.linalg.norm([(v[i]**2/mu-1/r[i])*R[i]-(np.dot(R[i],V[i])*V[i])/mu for i in range(len(W))], axis=1)
+L = np.cross(R,V)
+i_total = np.arccos(L[:,2]/np.linalg.norm(L, axis=1))
 E_total = v**2/2-mu/r
-
 a_total = -mu/2/E_total
+# %%
+genga_data = np.array(pd.read_csv('./incCN.csv'))
+genga_t = genga_data[:,0]
+genga_e = genga_data[:,1]
+genga_i = genga_data[:,2]
 
-# e_results_low = np.zeros(noutputs)
-# e_results_mid = np.zeros(noutputs)
-e_results_high = np.zeros(noutputs)
-
-# e_results_low = np.zeros(noutputs)
-# e_results_mid = np.zeros(noutputs)
-i_results_high = np.zeros(noutputs)
-
-a_results = np.zeros(noutputs)
+e_results = np.zeros(noutputs)
+i_results = np.zeros(noutputs)
 
 for j in range(noutputs):    
-    ehat = e/h
+    ehat = e/h    
     ihat = i/h
-    
-    tau_e = t_wave/0.78*(1+(1/15)*ehat**3)              # equation 34 from Ida 20
-    # tau_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)   # equation 11 from Creswell+Nelson 08
-    
-    tau_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)
-    
-    de = -e/tau_e*dt
+    # t_e = t_wave/0.78*(1+1/15*(ehat**2+ihat**2)**(3/2))                     # equation 34 from Ida 20
+    # t_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))                  # Ida
+    t_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)     # equation 11 from Creswell+Nelson 08
+    t_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)    # CN    
+    de = -e/t_e*dt
     e = e+de
-    # e_results_low[j] = e
-    # e_results_mid[j] = e
-    e_results_high[j] = e
-    
-    di = -i/tau_i*dt
+    e_results[j] = e    
+    di = -i/t_i*dt
     i = i+di
-    # e_results_low[j] = e
-    # e_results_mid[j] = e
-    i_results_high[j] = i
-    
-    # tau_m = 1/((2.7+1.1*0.5)/(2)*h**2*(1-(ehat/2.02)**4)/(1+(ehat/2.25)**0.5+(ehat/2.84)**6)/t_wave)
-    # tau_a  = 1/(2/tau_m+2*e**2/tau_e)
-    
-    # da = -a/t_a*dt
-    # a = a+da
-    # a_results[j] = a
+    i_results[j] = i    
+# %%
+fig, ax = plt.subplots(1, figsize=(7,5))
+# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_results, label="euler")
+# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_total, linestyle='--', label="RK")
+ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_results, c='tab:blue')
+ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_total, linestyle='-', c='tab:orange')
+ax.plot(genga_t, genga_i, linestyle='--', c='tab:pink', label='genga')
+ax.axhline(h, c='black', label='e = H/r')
+ax.set_xlabel('time (years)')
+ax.set_ylabel('e')
+ax.set_xlim(0, noutputs*dt/2/np.pi*omegak)
+ax.set_ylim(0)
+ax.tick_params(which='both', direction="in", top=True, right=True)
+ax.grid()
+ax.legend()
+
+# fig.savefig('/home/john/Desktop/summerproject/img/num+analyticIDA.pdf', bbox_inches='tight')
 # %%
 times = np.linspace(0,totaltime,noutputs) # for displaying time elapsed in plot
 fig, ax = plt.subplots(1, figsize=(9, 9))
@@ -217,27 +177,9 @@ def animate(i):
     planetline.set_data(W[0:i,1],W[0:i,2])
     text.set_text('{:.1f} years'.format(times[i]))
     return star, planet, planetline, text
-    
+
 anim = animation.FuncAnimation(fig, animate, frames=noutputs, interval=1, blit=True)
-# %%
-fig, ax = plt.subplots(1, figsize=(7,5))
 
-# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_results_low, label='analytical', c='tab:blue')
-# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_total_low, linestyle='--', label='numerical', c='tab:orange')
-ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_results_high, c='tab:blue')
-ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_total_high, linestyle='--', c='tab:orange')
-ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_results_high, c='tab:blue')
-ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_total, linestyle='--', c='tab:orange')
-ax.axhline(h, c='black', label='e = H/r')
-ax.set_xlabel('time (years)')
-ax.set_ylabel('e')
-ax.set_xlim(0, noutputs*dt/2/np.pi*omegak)
-ax.set_ylim(0)
-ax.tick_params(which='both', direction="in", top=True, right=True)
-ax.grid()
-ax.legend()
-
-# fig.savefig('/home/john/Desktop/summerproject/img/num+analyticIDA.pdf', bbox_inches='tight')
 # %%
 
 fig, ax = plt.subplots(1, figsize=(8, 8))
@@ -250,6 +192,6 @@ ax.set_ylabel('Distance (AU)')
 ax.grid()
 ax.scatter(0,0, c='black')
 ax.scatter(W[-1,1], W[-1,2], c='steelblue')
-ax.plot(W[:,1], W[:,2], linewidth=0.01, c='steelblue')
+ax.plot(W[:,1], W[:,2], linewidth=0.05, c='steelblue')
 
 # plt.savefig('/home/john/Desktop/summerproject/img/5000years.png', bbox_inches='tight')
