80b0788490af6a22b96ae193ed10846627b1cb7f
John Gillan <jgillan@protonmail.com>
2020-07-27 17:06:15 +0200

    removed integrator file

--- rungekutta.py
+++ rungekutta.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from mpl_toolkits.mplot3d import axes3d
 from timeit import default_timer as timed
 
 year = 365.25*24.*60.*60.
@@ -24,11 +25,11 @@ r = 1
 sigma = 17000*r**(-1/2)*au**2/mstarkg
 lim = 1.2
 mu = G*mstar
-noutputs = 100000
+noutputs = 10000
 totaltime = noutputs*dt
 h = 0.05
 e = 0.0
-i = 0.07
+i = 0.14
 ehat = e/h
 ihat = i/h
 r_p = r*(1-e) # perihelion
@@ -41,6 +42,8 @@ vorb = np.sqrt(mu*(2/r_a-1/r)) # instantaneous orbital speed at aphelion
 W0 = np.array([0,y,z,-vorb,0,0])
 omegak = np.sqrt(mu/r**3)
 t_wave = mstar/mplanet*mstar/sigma/r**2*h**4/omegak
+
+ida = True # select prescription
 # %%
 def acceleration(W0):
     R = W0[0:3]                             # position vector
@@ -52,40 +55,35 @@ def acceleration(W0):
     omegak = np.sqrt(mu/r**3)               
     t_wave = mstar/mplanet*mstar/sigma/r**2*h**4/omegak
     
-    theta = np.arctan2(R[1],R[0])
-    # print(v)
-    # print(vk)
-    
+    theta = np.arctan2(R[1],R[0])                           # angle of rotation about z axis
     u_r = np.array([np.cos(theta), np.sin(theta), 0])       # radial unit vector
     u_a = np.array([-np.sin(theta), np.cos(theta), 0])      # azimuthal unit vector
     u_z = np.array([0,0,1])
 
-    vr = np.dot(V,u_r)                     # radial velocity
-    va = np.dot(V,u_a)                   # azimuthal velocity
+    vr = np.dot(V,u_r)                      # radial velocity
+    va = np.dot(V,u_a)                      # azimuthal velocity
     vz = np.dot(V,u_z)
-    
-    # print(u_r)
-    # print(V-(vr*u_r+va*u_a))
      
-    L = np.cross(R,V)
-    i = np.arccos(L[2]/np.linalg.norm(L))
+    L = np.cross(R,V)  # angular momentum
+    i = np.arccos(L[2]/np.linalg.norm(L)) # inclination
     ihat = i/h    
     e = np.linalg.norm((v**2/mu-1/r)*R-np.dot(R,V)/mu*V) # eccentricity
     ehat = e/h    
     
-    # Ida prescription
-    # t_a = t_wave/Ct/h**2*(1+Ct/Cm*(ehat**2+ihat**2)**(1/2))
-    # t_e = t_wave/0.78*(1+1/15*(ehat**2+ihat**2)**(3/2))
-    # t_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))
-    # dvdt2 = - vk/2/t_a*u_a - vr/t_e*u_r - (va-vk)/t_e*u_a - vz/t_i*u_z
-    
-    # CN prescription
-    Pe = (1+(ehat/2.25)**1.2+(ehat/2.84)**6)/(1-(ehat/2.02)**4)
-    t_a = t_wave*2/h**2/(2.7+1.1*0.5)*(Pe+Pe/np.abs(Pe)*(0.07*ihat+0.085*ihat**4-0.08*ehat*ihat**2))
-    t_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)
-    t_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)    
-    dvdt2 = - V/t_a - 2*np.dot(V,R)*R/r**2/t_e - vz/t_i*u_z
-    
+    if ida:
+        # Ida prescription
+        t_a = t_wave/Ct/h**2*(1+Ct/Cm*(ehat**2+ihat**2)**(1/2))
+        t_e = t_wave/0.78*(1+1/15*(ehat**2+ihat**2)**(3/2))
+        t_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))
+        dvdt2 = - vk/2/t_a*u_a - vr/t_e*u_r - (va-vk)/t_e*u_a - vz/t_i*u_z
+    else:
+        # CN prescription
+        Pe = (1+(ehat/2.25)**1.2+(ehat/2.84)**6)/(1-(ehat/2.02)**4)
+        t_a = t_wave*2/h**2/(2.7+1.1*0.5)*(Pe+Pe/np.abs(Pe)*(0.07*ihat+0.085*ihat**4-0.08*ehat*ihat**2))
+        t_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)
+        t_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)    
+        dvdt2 = - V/t_a - 2*np.dot(V,R)*R/r**2/t_e - vz/t_i*u_z
+        
     return np.hstack((V, dvdtG+dvdt2))
 
 def rungekutta(W0):
@@ -108,53 +106,53 @@ print(timed()-timer)
 
 R = W[:,0:3]
 V = W[:,3:6]
-theta = np.arctan2(R[:,1],R[:,0])
 r = np.linalg.norm(R, axis=1)
 v = np.linalg.norm(V, axis=1)
-e_total = np.linalg.norm([(v[i]**2/mu-1/r[i])*R[i]-(np.dot(R[i],V[i])*V[i])/mu for i in range(len(W))], axis=1)
-L = np.cross(R,V)
-i_total = np.arccos(L[:,2]/np.linalg.norm(L, axis=1))
-E_total = v**2/2-mu/r
-a_total = -mu/2/E_total
+e_rk = np.linalg.norm([(v[i]**2/mu-1/r[i])*R[i]-(np.dot(R[i],V[i])*V[i])/mu for i in range(len(W))], axis=1)
+L = np.cross(R,V)  # angular momentum
+i_rk = np.arccos(L[:,2]/np.linalg.norm(L, axis=1))
 # %%
-genga_data = np.array(pd.read_csv('./incCN.csv'))
+genga_data = np.array(pd.read_csv('./data_high_i_ida.csv'))
 genga_t = genga_data[:,0]
 genga_e = genga_data[:,1]
 genga_i = genga_data[:,2]
 
-e_results = np.zeros(noutputs)
-i_results = np.zeros(noutputs)
+e_euler = np.zeros(noutputs)
+i_euler = np.zeros(noutputs)
 
 for j in range(noutputs):    
     ehat = e/h    
     ihat = i/h
-    # t_e = t_wave/0.78*(1+1/15*(ehat**2+ihat**2)**(3/2))                     # equation 34 from Ida 20
-    # t_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))                  # Ida
-    t_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)     # equation 11 from Creswell+Nelson 08
-    t_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)    # CN    
+    if ida:
+        t_e = t_wave/0.78*(1+1/15*(ehat**2+ihat**2)**(3/2))                     # equation 34 from Ida 20
+        t_i = t_wave/0.544*(1+1/21.5*(ehat**2+ihat**2)**(3/2))                  # Ida
+    else:
+        t_e = t_wave/0.78*(1-0.14*ehat**2+0.06*ehat**3+0.18*ehat*ihat**2)     # equation 11 from Creswell+Nelson 08
+        t_i = t_wave/0.544*(1-0.30*ihat**2+0.24*ihat**3+0.14*ihat*ehat**2)    # CN    
     de = -e/t_e*dt
     e = e+de
-    e_results[j] = e    
+    e_euler[j] = e    
     di = -i/t_i*dt
     i = i+di
-    i_results[j] = i    
+    i_euler[j] = i    
 # %%
-fig, ax = plt.subplots(1, figsize=(7,5))
-# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_results, label="euler")
-# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_total, linestyle='--', label="RK")
-ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_results, c='tab:blue')
-ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_total, linestyle='-', c='tab:orange')
-ax.plot(genga_t, genga_i, linestyle='--', c='tab:pink', label='genga')
+fig, ax = plt.subplots(1, figsize=(9,7))
+# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_euler, label="euler")
+# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, e_rk, linestyle='--', label="RK")
+ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_euler, c='tab:blue', label=r"$t_{inc}$")
+# ax.plot(np.arange(0,noutputs,1)*dt/2/np.pi*omegak, i_rk, linestyle='-', c='tab:orange', label="RK")
+ax.plot(genga_t, genga_i, linestyle='--', c='tab:red', label='genga')
 ax.axhline(h, c='black', label='e = H/r')
 ax.set_xlabel('time (years)')
-ax.set_ylabel('e')
+ax.set_ylabel('i')
 ax.set_xlim(0, noutputs*dt/2/np.pi*omegak)
 ax.set_ylim(0)
+ax.set_title("Ida")
 ax.tick_params(which='both', direction="in", top=True, right=True)
 ax.grid()
 ax.legend()
 
-# fig.savefig('/home/john/Desktop/summerproject/img/num+analyticIDA.pdf', bbox_inches='tight')
+# fig.savefig('/home/john/summerproject/img/genga_high_i_ida.png', bbox_inches='tight')
 # %%
 times = np.linspace(0,totaltime,noutputs) # for displaying time elapsed in plot
 fig, ax = plt.subplots(1, figsize=(9, 9))
@@ -181,7 +179,6 @@ def animate(i):
 anim = animation.FuncAnimation(fig, animate, frames=noutputs, interval=1, blit=True)
 
 # %%
-
 fig, ax = plt.subplots(1, figsize=(8, 8))
 ax.set_title('{:.0f} years'.format(totaltime))
 ax.set_xlim(-lim, lim)
@@ -195,3 +192,20 @@ ax.scatter(W[-1,1], W[-1,2], c='steelblue')
 ax.plot(W[:,1], W[:,2], linewidth=0.05, c='steelblue')
 
 # plt.savefig('/home/john/Desktop/summerproject/img/5000years.png', bbox_inches='tight')
+# %%
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, projection='3d')
+lim = 0.8
+ax.scatter(0,0,0, c='black')
+ax.scatter(W[-1,0], W[-1,1], W[-1,2], c='steelblue')
+ax.plot(W[0::1,0], W[0::1,1], W[0::1,2], linewidth=0.005, c='steelblue')
+ax.view_init(elev=2, azim=340)
+ax.set_xlabel('X (AU)')
+ax.set_ylabel('Y (AU)')
+ax.set_zlabel('Z (AU)')
+ax.set_xlim([-lim,lim])
+ax.set_ylim([-lim,lim])
+ax.set_zlim([-lim,lim])
+
+# plt.savefig(f'close-{particles}-{runtime}-{ts}.png', bbox_inches='tight')
+
