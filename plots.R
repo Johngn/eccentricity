ed417f95c74d4a9e407129bdd1944fb09877dc28
John Gillan <jgillan@protonmail.com>
2020-08-07 22:22:36 +0200

    *

--- plots.R
+++ plots.R
@@ -6,22 +6,23 @@ cn <- read_csv("summerproject/data/datatestcn.csv")
 names(ida) <- c("planet","time","a","eccentricity","inclination")
 names(cn) <- c("planet","time","a","eccentricity","inclination")
 
-ida %>%
-  ggplot(aes(x = a, y = eccentricity)) +
-  geom_point() +
-  geom_point(cn)
-  labs(title = "")
-
 ggplot() +
   geom_point(data = ida, 
              aes(x = a, y = eccentricity),
              color = 'orange') +
   geom_point(data = cn,
              aes(x = a, y = eccentricity),
-             color = 'blue') +
+             color = 'dodgerblue1') +
   geom_smooth(data = ida, 
               aes(x = a, y = eccentricity),
               color = 'orange') +
   geom_smooth(data = cn,
               aes(x = a, y = eccentricity),
-              color = 'blue')
+              color = 'dodgerblue1')
+
+ida %>%
+  ggplot(aes(n, a)) +
+  geom_col()
+  
+ggplot(data = ida) +
+  geom_bar(mapping = aes(x = a))
