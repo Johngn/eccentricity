fe613b291d0818327e75b67f26a30c08e1e9fa83
John Gillan <jgillan@protonmail.com>
2020-08-09 22:38:12 +0200

    plots now use dat file data

--- genga_plots.py
+++ genga_plots.py
@@ -12,46 +12,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import axes3d
-from timeit import default_timer as timed
-from glob import glob
 # %%
-ida1 = np.array(pd.read_csv('./data/datatestida.csv'))
-# cn1 = np.array(pd.read_csv('./data/datatestcn.csv'))
-# ida2 = np.array(pd.read_csv('./data/datatestida2.csv'))
-# cn2 = np.array(pd.read_csv('./data/datatestcn2.csv'))
-# idal = np.array(pd.read_csv('./data/datatestidalarge.csv'))
-# cnl = np.array(pd.read_csv('./data/datatestcnlarge.csv'))
-# genga_p = genga_data[:,0]
-# genga_t = genga_data[:,1]
-# genga_a = genga_data[:,2]
-# genga_e = genga_data[:,3]
-# genga_i = genga_data[:,4]
-total_time = int(ida1[0,1])
+data = np.loadtxt('./data/Outida_000001000000.dat')
+
+k = 0.0172020989
+G = k**2
+days_per_year = 365.25
+
+mass = data[0][2]
+mu = G
+
+p = [data[i][1] for i in range(len(data))]
+t = [data[i][0] for i in range(len(data))]
+R = [data[i][4:7] for i in range(len(data))]
+V = [data[i][7:10]*k for i in range(len(data))]
+r = np.linalg.norm(R, axis=1)
+v = np.linalg.norm(V, axis=1)
+L = np.cross(R,V)
+i = np.arccos(L[:,2]/np.linalg.norm(L, axis=1))
+e = np.linalg.norm([(v[i]**2/mu - 1/r[i])*R[i] - np.dot(R[i],V[i])*V[i]/mu for i in range(len(data))], axis=1)
+E = v**2/2 - mu/r
+a = -mu/2/E
 
 fig, ax = plt.subplots(1, figsize=(9,7))
-ax.scatter(ida1[:,2], ida1[:,3], s=20, alpha=0.7, label='IDA')
-ax.scatter(cn1[:,2], cn1[:,3], s=20, alpha=0.7, label='CN')
+ax.scatter(a, e, s=20, alpha=0.7, label='IDA')
+# ax.scatter(cn1[:,2], cn1[:,3], s=20, alpha=0.7, label='CN')
 # ax.scatter(ida2[:,2], ida2[:,3], s=20, alpha=0.7, label='IDA')
 # ax.scatter(cn2[:,2], cn2[:,3], s=20, alpha=0.7, label='CN')
-ax.scatter(idal[:,2], idal[:,3], s=20, alpha=0.7, label='IDA')
-ax.scatter(cnl[:,2], cnl[:,3], s=20, alpha=0.7, label='CN')
+# ax.scatter(idal[:,2], idal[:,3], s=20, alpha=0.7, label='IDA')
+# ax.scatter(cnl[:,2], cnl[:,3], s=20, alpha=0.7, label='CN')
 ax.set_xlabel('semi-major axis (AU)')
 ax.set_ylabel('eccentricity')
 ax.set_ylim(0)
-ax.set_title(f'Eccentricity and semi-major axis after {total_time} years')
+ax.set_title(f'Eccentricity and semi-major axis after {np.round(t[-1],0)} years')
 ax.legend()
 
 fig, ax = plt.subplots(1, figsize=(9,7))
-ax.scatter(ida1[:,2], ida1[:,4], s=20, alpha=0.7, label='IDA')
-ax.scatter(cn1[:,2], cn1[:,4], s=20, alpha=0.7, label='CN')
+ax.scatter(a, i, s=20, alpha=0.7, label='IDA')
+# ax.scatter(cn1[:,2], cn1[:,4], s=20, alpha=0.7, label='CN')
 # ax.scatter(ida2[:,2], ida2[:,4], s=20, alpha=0.7, label='IDA')
 # ax.scatter(cn2[:,2], cn2[:,4], s=20, alpha=0.7, label='CN')
-ax.scatter(idal[:,2], idal[:,4], s=20, alpha=0.7, label='IDA')
-ax.scatter(cnl[:,2], cnl[:,4], s=20, alpha=0.7, label='CN')
+# ax.scatter(idal[:,2], idal[:,4], s=20, alpha=0.7, label='IDA')
+# ax.scatter(cnl[:,2], cnl[:,4], s=20, alpha=0.7, label='CN')
 ax.set_xlabel('semi-major axis (AU)')
 ax.set_ylabel('inclination')
 ax.set_ylim(0)
-ax.set_title(f'Inclination and semi-major axis after {total_time} years')
+ax.set_title(f'Inclination and semi-major axis after {np.round(t[-1],0)} years')
 ax.legend()
 # %%
 idaR = np.loadtxt('./data/Outida2_000000000000.dat')
@@ -68,11 +74,11 @@ ax.scatter(x, y, z, s=1, alpha=0.5)
 ax.set_xlabel('X (AU)')
 ax.set_ylabel('Y (AU)')
 ax.set_zlabel('Z (AU)')
-lim = 0.8
+lim = 1
 ax.set_xlim([-lim,lim])
 ax.set_ylim([-lim,lim])
 ax.set_zlim([-lim,lim])
 # %%
 
 fig, ax = plt.subplots(1, figsize=(9,6))
-ax.hist(genga_ida[:,2], 20)
+ax.hist(i, 20)
