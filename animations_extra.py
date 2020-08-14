8eb9531e215ce309d47f7f042959440e084087f7
John Gillan <jgillan@protonmail.com>
2020-08-14 10:52:00 +0200

    finished hist animation

--- animations_extra.py
+++ animations_extra.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 11:01:12 2020
+
+@author: johngillan
+"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, FFMpegWriter
-
-data_ida = np.array(pd.read_csv('./data/dataida_mars_4.csv'))
-data_cn = np.array(pd.read_csv('./data/datacn_mars_4.csv'))
 # %%
+data_ida = np.array(pd.read_csv('./data/dataida_mars_3.csv'))
+data_cn = np.array(pd.read_csv('./data/datacn_mars_3.csv'))
+
 data_ida_new = np.ones((5050101, 6))*-1
 data_cn_new = np.ones((5050101, 6))*-1
 
@@ -27,70 +35,90 @@ for i in range(101):
     data_mask[i:101*mask_len:101] = True
     data_cn_new[data_mask] = data_cn[planet0_mask]
     
-data_ida_new = np.reshape(data_ida_new, (-1, 101, 6))[::100]
-data_cn_new = np.reshape(data_cn_new, (-1, 101, 6))[::100]
+data_ida = np.reshape(data_ida_new, (-1, 101, 6))[::100]
+data_cn = np.reshape(data_cn_new, (-1, 101, 6))[::100]
 # %%
-times = data_ida_new[:,0,1]
+solar_to_earth_mass = 1.99e30/5.97e24
+times = data_ida[:,0,1]
 
-fig, axes = plt.subplots(2, figsize=(8, 8))
-axes[0].set_xlim(-0,6)
+fig, axes = plt.subplots(3, figsize=(8, 10))
+# fig.subplots_adjust(hspace=0)
+axes[0].set_xlim(-0,7)
 axes[0].set_ylim(0,0.15)
 axes[0].set_ylabel("eccentricity")
 
-axes[1].set_xlim(-0,6)
+axes[1].set_xlim(-0,7)
 axes[1].set_ylim(0,0.15)
-axes[1].set_xlabel("semi-major axis")
 axes[1].set_ylabel("inclination")
+
+axes[2].set_xlim(-0,7)
+axes[2].set_ylim(0,2.5)
+axes[2].set_xlabel("semi-major axis")
+axes[2].set_ylabel("mass")
 
 planets_ida_e, = axes[0].plot([], [], "o", markersize=8, alpha=0.8, label="Ida")
 planets_cn_e, = axes[0].plot([], [], "o", markersize=8, alpha=0.8, label="CN")
+
 planets_ida_i, = axes[1].plot([], [], "o", markersize=8, alpha=0.8, label="Ida")
 planets_cn_i, = axes[1].plot([], [], "o", markersize=8, alpha=0.8, label="CN")
+
+planets_ida_m, = axes[2].plot([], [], "o", markersize=8, alpha=0.8, label="Ida")
+planets_cn_m, = axes[2].plot([], [], "o", markersize=8, alpha=0.8, label="CN")
 text = axes[0].text(2.2, 0.12, '', fontsize=15)
 
 axes[0].legend()
 axes[1].legend()
+axes[2].legend()
 
 def animate(i):
-    planets_ida_e.set_data(data_ida_new[i,:,3], data_ida_new[i,:,4])
-    planets_cn_e.set_data(data_cn_new[i,:,3], data_cn_new[i,:,4])
-    planets_ida_i.set_data(data_ida_new[i,:,3], data_ida_new[i,:,5])
-    planets_cn_i.set_data(data_cn_new[i,:,3], data_cn_new[i,:,5])
+    planets_ida_e.set_data(data_ida[i,:,3], data_ida[i,:,4])
+    planets_cn_e.set_data(data_cn[i,:,3], data_cn[i,:,4])
+    
+    planets_ida_i.set_data(data_ida[i,:,3], data_ida[i,:,5])
+    planets_cn_i.set_data(data_cn[i,:,3], data_cn[i,:,5])
+    
+    planets_ida_m.set_data(data_ida[i,:,3], data_ida[i,:,2]*solar_to_earth_mass)
+    planets_cn_m.set_data(data_cn[i,:,3], data_cn[i,:,2]*solar_to_earth_mass)
+    
     text.set_text(r'{} $\times 10^3$ years'.format(int(times[i]/1e3)))
     return planets_ida_e, planets_cn_e, planets_ida_i, planets_cn_i
     
-im_ani = FuncAnimation(fig, animate, frames=len(data_ida_new), interval=1)
+im_ani = FuncAnimation(fig, animate, frames=len(data_ida), interval=1)
 # %%
-solar_to_earth_mass = 1.99e30/5.97e24
+plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+f = f'vid/genga_scatter_3.mp4' 
+writervideo = FFMpegWriter(fps=30) # ffmpeg must be installed
+im_ani.save(f, writer=writervideo)
+# %%
+data_all = [data_ida, data_cn]
 
-fig, axes = plt.subplots(2, figsize=(8,8))
-# fig.subplots_adjust(hspace=0.4)
-# i = 10
-# markers = data_ida_new[-1,:,2]*solar_to_earth_mass*200
-# plt.scatter(data_ida_new[0,:,3], data_ida_new[0,:,4], s=markers)
+fig, axes = plt.subplots(3, figsize=(8,12))
+fig.subplots_adjust(hspace=0.4)
 
-def animate(i, data_ida_new):
+def animate(i, data_all):
     axes[0].cla()
+    axes[0].set_xlabel("semi-major axis")
+    axes[0].set_ylim(0,20)
+    axes[0].hist([data_all[0][i,:,3], data_all[1][i,:,3]], bins=40, range=(0,7), label=['IDA', 'CN'])
     axes[0].set_title(r'{} $\times 10^3$ years'.format(int(times[i]/1e3)))
-    axes[0].set_xlim(-0,6)
-    axes[0].set_ylim(0,0.15)
-    markers = data_ida_new[0,:,2]*solar_to_earth_mass*200
-    # print(markers[0])
+    axes[0].legend()
     
-    axes[0].scatter(data_ida_new[i,:,3], data_ida_new[i,:,4], s=markers, alpha=0.8, label="Ida")
+    axes[1].cla()
+    axes[1].set_xlabel("eccentricity")
+    axes[1].set_ylim(0,20)
+    axes[1].hist([data_all[0][i,:,4], data_all[1][i,:,4]], bins=40, range=(0,0.06), label=['IDA', 'CN'])
+    axes[1].legend()
     
-    # axes[1].cla()
-    # axes[1].plot(data_cn_new[i,:,3], data_cn_new[i,:,4], "o", markersize=8, alpha=0.8, label="CN")
+    axes[2].cla()
+    axes[2].set_xlabel("inclination")
+    axes[2].set_ylim(0,20)
+    axes[2].hist([data_all[0][i,:,5], data_all[1][i,:,5]], bins=40, range=(0,0.06), label=['IDA', 'CN'])
+    axes[2].legend()
 
-animation = FuncAnimation(fig, animate, frames=len(data_ida_new), fargs=(data_ida_new,))
-
+animation = FuncAnimation(fig, animate, frames=len(data_ida), fargs=(data_all,), interval=1)
 # %%
-x = [1,2,3,4,5]
-y = [a**2 for a in x]
-s = np.array([10*4**n for n in range(len(x))])
-plt.scatter(x,y,s=s)
-plt.title('Doubling width of marker in scatter plot')
-plt.xlabel('x')
-plt.ylabel('x**2')
-plt.xlim(0,6)
-plt.ylim(0,30)
+plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+writervideo = FFMpegWriter(fps=30) # ffmpeg must be installed
+animation.save('vid/genga_hist_3.mp4', writer=writervideo)
+
+# animation.save('vid/genga_hist_4.gif', writer='imagemagick', fps=60)
